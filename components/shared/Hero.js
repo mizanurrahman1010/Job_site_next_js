5f871eca9cbbca384ff1882e3e9a95a671616c2b
Mizan <mizanurrahman.reyad@gmail.com>
2021-10-31 17:05:27 +0600

    ui

---
+++ components/shared/Hero.js
@@ -0,0 +1,24 @@
+
+const Hero = () =>
+  <section className="fj-hero">
+    <div className="fj-hero-wrapper row">
+      <div className="hero-left col-md-6">
+        <h1 className="white hero-title">Here you can find your perfect employement</h1>
+        <h2 className="white hero-subtitle">Our Job Site</h2>
+        <div className="button-container">
+          <a href="" className="btn btn-main bg-blue ttu">Explore Us</a>
+        </div>
+      </div>
+      <div className="hero-right col-md-6">
+        <div className="hero-image-container">
+          <a className="grow hero-link">
+            <img
+              className="hero-image"
+              src="https://www.lbbd.gov.uk/sites/default/files/images/shutterstock_1055044370.jpg"></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+export default Hero;
---
+++ pages/cv.js
@@ -0,0 +1,14 @@
+
+import { Section, PageTitle } from 'components/shared';
+
+const CV = () => {
+  return (
+    <Section>
+      <PageTitle text="CV" />
+      New Page
+    </Section>
+  )
+}
+
+export default CV;
+
